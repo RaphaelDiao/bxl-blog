cf3cfd349a92eb91dfba54977b9d42c68e95d251
RaphaelDiao <394749334@qq.com>
2019-04-20 15:50:30 +0800

    Initial commit

---
+++ src/main/java/com/yonghui/bxlblog/BxlBlogApplication.java
@@ -0,0 +1,13 @@
+package com.yonghui.bxlblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BxlBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BxlBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yonghui/bxlblog/BxlBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yonghui.bxlblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BxlBlogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
